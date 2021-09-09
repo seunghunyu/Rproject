d1a6ee13b76b7cfd14e5cf5de60391f2dd1ce6ec
ysh <livefor1212@gmail.com>
2021-09-09 19:37:55 +0900

    commit

---
+++ 2Day.R
@@ -0,0 +1,25 @@
+ library("leaflet")
+ Comment1 = "
+m <- leaflet() 
+  addTiles() 
+  addMarkers(lng=174.768, lat = 36.852)
+m # print the map
+
+
+
+
+leaflet() %>%
+setView(lng=126.9784, lat=37.566, zoom=11) %>%!
+addTiles() 
+
+" 
+
+
+# calculator
+1+2*(3+4)
+ 
+log(43+32+1)
+
+((4+3)*(2+1))^0.5
+
+
